ba68678d26c94089d79bf32f7315ddb787da06c8
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-07-20 10:53:26 +0300

    feat: load editor bundle when installed

--- php-backend/views/document/form.php
+++ php-backend/views/document/form.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use app\assets\EditorAsset;
 use app\models\Document;
 use yii\bootstrap5\ActiveForm;
 use yii\helpers\Html;
@@ -17,6 +18,11 @@ $cancelUrl = $model->isNewRecord
     ? ($model->collection_id ? ['/collection/view', 'id' => $model->collection_id] : ['/site/dashboard'])
     : ['/document/view', 'id' => $model->id];
 $contentJson = Json::encode($model->getContentData());
+$editorJs = Yii::getAlias('@webroot/editor/outline-editor.js');
+$editorCss = Yii::getAlias('@webroot/editor/outline-editor.css');
+if (is_file($editorJs) && is_file($editorCss)) {
+    EditorAsset::register($this);
+}
 ?>
 <div class="document-form-shell">
     <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-4">
